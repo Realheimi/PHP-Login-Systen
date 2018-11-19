e87e5f9ea2ea51dff552423beccde00ea9e79cee
Heimeshoff <joern.heimeshoff@wittmann-group.com>
2018-11-19 19:50:26 +0100

    Login2

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Wenn keine Konstante __CONFIG__ definiert ist, wird die Config nicht geladen
+    if (!defined('__CONFIG__')) {
+        Exit('Non Config Available');
+    }
+
+    //erlaubte inc Dateien
+    define('FOOTERALLOWED', true);
+
+
+    // Hier Startet die Config
+?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+    // Die Config erlauben
+    define('__CONFIG__', true);
+    // Die Config laden
+    require_once "inc/config.php"; ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -35,7 +42,7 @@
   </div>
 </div>
   
-
+    <?php require_once "inc/footer.php"; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
