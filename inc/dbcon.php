c7cad629066b3d31dc76cfaaba557a1e9ed38d45
Heimeshoff <joern.heimeshoff@wittmann-group.com>
2018-11-20 07:51:54 +0100

    dbcon - für Datenbank Zugang ohne Github

---
+++ inc/dbcon.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Wenn keine Konstante __CONFIG__ definiert ist, wird die Config nicht geladen
+    if (!defined('__CONFIG__')) {
+        Exit('Non Config Available');
+    }
+
+    // Hier die Zugangsdaten zur Datenbank getrennt, damit sie nicht auf GitHub landen.
+
+?>
